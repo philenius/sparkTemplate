fa927b8c09d6a4a497a57d1c8b3c3d930c267fb0
philenius <philippus-perez@web.de>
2017-05-03 17:46:07 +0200

    added package and object with main method

---
+++ src/main/scala/de/bigdata/spark/App.scala
@@ -0,0 +1,10 @@
+package de.bigdata.spark
+
+/**
+  * IntelliJ IDEA template for Apache Spark Standalone App with Scala and Maven.
+  */
+object App {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
