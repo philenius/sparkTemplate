e6b4e8098010c8b3a4d904075238bff6f72ea07a
philenius <philippus-perez@web.de>
2017-05-03 17:47:16 +0200

    added code for retrieving SparkSession

--- src/main/scala/de/bigdata/spark/App.scala
+++ src/main/scala/de/bigdata/spark/App.scala
@@ -1,10 +1,16 @@
 package de.bigdata.spark
 
+import org.apache.spark.sql.SparkSession
+
 /**
   * IntelliJ IDEA template for Apache Spark Standalone App with Scala and Maven.
   */
 object App {
   def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder().master("local").appName("spark-test").getOrCreate()
+
+    spark.stop()
 
   }
 }
